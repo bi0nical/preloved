c2a74a4cece7c34c28f78bee1843c607044088f9
rory <turnburory@gmail.com>
2022-04-04 15:56:32 +1200

    testing listings

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,9 @@
    const cors = require('cors'); // Includes cors
    const config = require('./config.json');
    //Database collection variables (product is for testing, user is for stretch goal and portfolios is the one we are using)
-   const Product = require('./models/product.js');
-   const User = require('./models/user.js');
-   const Portfolios = require('./models/portfolios');
+   const Listing = require('./models/listings.js');
+   const User = require('./models/users.js');
+   const Comment = require('./models/comments.js');
    
    
    // Backend port number
@@ -39,3 +39,17 @@ mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@pr
 // <--! port listening code starts here -->
 app.listen(port,()=>console.log(`My fullstack application is listening on port ${port}`))
 // <--! port listening code ends here -->
+
+
+// TESTING! WARNING TESTING FUNCTION! TESTING ONLY!
+app.post('/addListing',(req,res)=>{
+    const dbListing = new Listing({
+      _id: new mongoose.Types.ObjectId,
+      name: req.body.name,  
+    });
+    // save to database and to notify the user
+    dbListing.save().then(result=>{
+      res.send(result);
+    }).catch(err=>res.send(err));
+  })
+//   TESTING! WARNING TESTING FUNCTION! TESTING ONLY!
