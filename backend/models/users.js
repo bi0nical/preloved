ccf1b0dd5fd66b115566d9330aac85b13e91c606
rory <turnburory@gmail.com>
2022-04-06 14:18:04 +1200

    all listing functions completed in test phase

--- backend/models/users.js
+++ backend/models/users.js
@@ -2,6 +2,22 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
+  username: String,
+  password: String,
+  profile_img: String,
+  email: String,
+  location: String,
+  rating: Number,
+  listings: 
+  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Listing'
+  },
+  comments:
+  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }
   
 
 })
